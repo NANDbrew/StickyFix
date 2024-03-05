94180b16965c3aded16f9f9239b470934a522370
NANDbrew <zortman@gmail.com>
2024-03-04 18:11:45 -0600

    Add project files.

---
+++ Patches.cs
@@ -0,0 +1,64 @@
+﻿using HarmonyLib;
+using SailwindModdingHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace StickyFix
+{
+    internal class ShipItemEmbarkPatches
+    {
+
+        [HarmonyPatch(typeof(ShipItem))]
+        private static class EmbarkPatches
+        {
+
+            [HarmonyPatch("Awake")]
+            [HarmonyPrefix]
+            public static void Awake(ShipItem __instance)
+            {
+                __instance.gameObject.AddComponent<EmbarkTracker>();
+            }
+
+            [HarmonyPatch("OnTriggerEnter")]
+            [HarmonyPrefix]
+            public static bool OnTriggerEnter(Collider other, ShipItem __instance, Collider ___currentBoatCollider)
+            {
+                List<Collider> colliders = __instance.GetComponent<EmbarkTracker>().embarkColliders;
+                if (other.CompareTag("EmbarkCol"))
+                {
+                    colliders.Add(other);
+                    if (___currentBoatCollider != null)
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+
+            [HarmonyPatch("OnTriggerExit")]
+            [HarmonyPostfix]
+            public static void OnTriggerExit(ShipItem __instance, Collider other, ref Collider ___currentlyStayedEmbarkCol)
+            {
+                List<Collider> colliders = __instance.GetComponent<EmbarkTracker>().embarkColliders;
+                colliders.Remove(other);
+
+                if (colliders.Count >= 1)
+                {
+                    ___currentlyStayedEmbarkCol = colliders[0];
+                }
+            }
+
+            [HarmonyPatch("EnterBoat")]
+            [HarmonyPrefix]
+            public static void EnterBoat(ShipItem __instance, Transform ___currentActualBoat)
+            {
+                __instance.InvokePrivateMethod("ExitBoat");
+            }
+
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using System;
+using System.Reflection;
+
+namespace StickyFix
+{
+    [BepInPlugin(PLUGIN_ID, PLUGIN_NAME, PLUGIN_VERSION)]
+    [BepInDependency("com.app24.sailwindmoddinghelper", "2.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string PLUGIN_ID = "com.nandbrew.stickyfix";
+        public const string PLUGIN_NAME = "StickyFix";
+        public const string PLUGIN_VERSION = "0.0.1";
+
+        //--settings--
+        //internal ConfigEntry<bool> someSetting;
+
+
+        private void Awake()
+        {
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PLUGIN_ID);
+
+            //someSetting = Config.Bind("Settings", "Some setting", false);
+        }
+    }
+}
